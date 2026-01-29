eca1599cafa2729d79811223cc69cb592770ea65
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-01-29 18:46:00 +0530

    hold input field and added an array to store all the url links in the future.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let inputBtn = document.querySelector("#input-btn");
+const inputBtn = document.querySelector("#input-btn");
+const inputEl = document.querySelector("#input-el");
+let myLeads = [];
 
 inputBtn.addEventListener("click", function () {
     console.log("button clicked!!!");
