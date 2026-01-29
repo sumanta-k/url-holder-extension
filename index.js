b80b959916d17bc27b326c83d7390258398409ca
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-01-29 18:54:02 +0530

    feature: pushing url manually for now though.

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const inputEl = document.querySelector("#input-el");
 let myLeads = [];
 
 inputBtn.addEventListener("click", function () {
-    console.log("button clicked!!!");
+    myLeads.push("https://github.com/sumanta-k/url-holder-extension");
 });
