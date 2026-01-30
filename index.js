96e695c630bb56e0807d1167bcbde5242fd144c0
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-01-30 19:13:46 +0530

    show user's what url's has been added to list

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const inputBtn = document.querySelector("#input-btn");
 let inputEl = document.querySelector("#input-el");
+let listUrl = document.querySelector("#list-url");
 let myLeads = [];
 
 inputBtn.addEventListener("click", function () {
     let userSearched = inputEl.value;
     myLeads.push(userSearched);
+
+    // TODO: list out url's
+    let li = document.createElement("li");
+    li.append(userSearched);
+    listUrl.append(li);
 
     inputEl.value = "";
     console.log(myLeads);
